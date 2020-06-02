b68e5885962c01f9aee64cb94c697e986658c92c
Jacob <reyno120@purdue.edu>
2020-06-02 09:31:06 -0400

    added react

---
+++ README.md
@@ -0,0 +1,2 @@
+# Event-Scheduler
+A basic event scheduler web application
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://jmreynolds:jmreynolds@mycluster-pjizw.mongodb.net/EventsScheduler?retryWrites=true&w=majority'
+};
